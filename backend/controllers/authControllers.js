d4c7de97b892a442a281f3b8632941d1c467a435
alerwann <alerwann411@gmail.com>
2025-07-26 17:06:30 +0200

    cookies

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -29,14 +29,16 @@ exports.login = async (req,res) =>
         { 
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
-          sameSite: 'strict'
+          sameSite: 'strict',
+          maxAge: 30 * 60 * 1000
         });
   
         res.cookie('refreshToken', refreshToken, 
         {
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
-          sameSite: 'strict'
+          sameSite: 'strict',
+           maxAge: 7 * 24 * 60 * 60 * 1000 // 7 jours en ms
         });
 
       res.json({ success: true,  user: { id: user._id, identifiant: user.identifiant , accessToken , refreshToken} });
--- frontutilisateur/src/services/api.js
+++ frontutilisateur/src/services/api.js
@@ -1,28 +1,57 @@
 import axios from 'axios';
 console.log(process.env.REACT_APP_API_URL)
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL });
+  baseURL: process.env.REACT_APP_API_URL,
+withCredentials:true});
+
+
+let isRefreshing =false;
+let failedRequestsQueue =[];
+
+
+api.interceptors.request.use((config) => {
+  
+  return config;
+});
+
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+    
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      try {
+        const { status } = await axios.post('/api/refresh', {}, { 
+          withCredentials: true 
+        });
+        
+        if (status === 200) return api(originalRequest);
+      } catch (refreshError) {
+        // Gestion propre des erreurs
+        if (refreshError.response?.status === 401) {
+          window.location.href = '/login?error=session_expired';
+        }
+        return Promise.reject(refreshError);
+      }
+    }
+    
+    return Promise.reject(error);
+  }
+);
 
 export const login = async (credentials) => {
   try {
-    const response = await api.post('/login', credentials);
+    const response = await api.post('/login', credentials, {
+      withCredentials: true
+    });
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || 'Échec de la connexion');
   }
 };
 
-// export const getCurrentUser = async () => {
-//   const token = localStorage.getItem('token');
-//   if (!token) return null;
+export const logout = async () => {
+  await api.post('/logout', {}, { withCredentials: true });
+  window.location.href = '/login';
+};
 
-  // try {
-  //   const response = await api.get('/api/me', {
-  //     headers: { 'Authorization': `Bearer ${token}` }
-  //   });
-  //   return response.data;
-  // } catch (error) {
-  //   localStorage.removeItem('token');
-  //   return null;
-  // }
-// };
