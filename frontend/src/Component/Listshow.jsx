be1c8cbabf6d4aa54a556c5c09f287692065d9c2
Alerwann <alerwann411@gmail.com>
2025-08-11 16:29:27 +0200

    suppression import inutile

--- frontend/src/Component/Listshow.jsx
+++ frontend/src/Component/Listshow.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+
 import { useNavigate } from "react-router-dom";
-import BackButton from "./BackButton";
+
 
 function ListUserShow(listUsers) {
   const navigate=useNavigate()
